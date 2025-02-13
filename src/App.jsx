901a5ae46152b6b0cb1cb18bd1fbbabfcb82b762
ONJANIAINA Henintsoa Stephana <ohenintsoa98@gmail.com>
2025-02-13 09:28:39 +0300

    Connexion de redux à react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import PhoneComponent from './components/PhoneComponent'
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
 function App() {
 	return (
-		<div ClassName="section-one">
-			<PhoneComponent/>
-		</div>
+		<Provider store={store}>
+			<div ClassName="section-one">
+				<PhoneComponent/>
+			</div>
+		</Provider>
 	)
 }
 
